80bb1de654c712e911174b756324eb3f06fe987a
stasiaaleks <alexeenko049@gmail.com>
2024-03-07 13:30:27 +0200

    Mapped keys and values from a file and added exception handling

--- cpp_assembly_demo/assembly_demo/FileReader.h
+++ cpp_assembly_demo/assembly_demo/FileReader.h
@@ -2,15 +2,41 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <vector>
+#include <map>
 using namespace std;
+
 
 static void ReadFile(string filepath)
 {
     string StringFromFile;
     ifstream ReadFile(filepath);
+    map<string, int> FileMap;
 
     while (getline(ReadFile, StringFromFile)) {
-        cout << StringFromFile << endl;
+        // cout << StringFromFile << endl; //output for test
+
+        stringstream ss(StringFromFile);
+        string key, valueStr;
+        int value;
+
+        ss >> key;
+        ss >> valueStr;
+
+        try {
+            value = stoi(valueStr);
+            if (value <= 10000 && value >= -10000)
+            {
+                FileMap[key] = value;
+            }
+            else cout << "Value out of range" << endl;
+            
+        }
+        catch (const std::invalid_argument& e) {
+            cout << "Invalid value argument" << endl;
+        }
+
     }
 
     ReadFile.close();
